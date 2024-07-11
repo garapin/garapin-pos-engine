f5f8f545189b9017cf6f32c863589e0f398eba2d
Prima <51253537+primajatnika27@users.noreply.github.com>
2024-07-12 04:13:22 +0700

    Update transactionEngine.js

--- src/engines/bagi-bagi-transaction/transactionEngine.js
+++ src/engines/bagi-bagi-transaction/transactionEngine.js
@@ -37,7 +37,7 @@ class TransactionEngine {
           "for-user-id": this.accountId,
         },
         params: {
-          limit: 15,
+          limit: 10,
           channel_categories: [ChannelCategory.VA, ChannelCategory.QR],
         },
       });
